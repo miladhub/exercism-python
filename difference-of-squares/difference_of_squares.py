a393b7f170b4fd722eb65d2dc32af154091a9e8f
Milad Bourhani <milad.bourhani@gmail.com>
2018-11-30 22:25:37 +0100

    diff of squares

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,8 @@
 def square_of_sum(count):
-    pass
-
+    return sum(range(1, count + 1)) ** 2
 
 def sum_of_squares(count):
-    pass
-
+    return sum(map(lambda x: x ** 2, range(1, count + 1)))
 
 def difference(count):
-    pass
+    return square_of_sum(count) - sum_of_squares(count)
